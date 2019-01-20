cd90ca3805c27fc5fc6bfd56edb1ab90b8428b1a
Florian Marcu <florian.mrc@gmail.com>
2019-01-20 12:39:05 -0800

    partially make it work

--- android/app/src/main/java/com/starterapp/MainActivity.java
+++ android/app/src/main/java/com/starterapp/MainActivity.java
@@ -1,9 +1,15 @@
-package com.starterapp;
+package io.instamobile.starterkit.rn.android;
 
+import android.content.Intent;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-
+	
+	@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
--- android/app/src/main/java/com/starterapp/MainApplication.java
+++ android/app/src/main/java/com/starterapp/MainApplication.java
@@ -1,17 +1,36 @@
-package com.starterapp;
+package io.instamobile.starterkit.rn.android;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import java.util.Arrays;
 import java.util.List;
+import android.support.multidex.MultiDexApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainApplication extends MultiDexApplication implements ReactApplication {
+  
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +41,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNFirebasePackage(),
+            new FastImageViewPackage(),
+            new FBSDKPackage(mCallbackManager),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseFirestorePackage()
       );
     }
 
@@ -40,6 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
